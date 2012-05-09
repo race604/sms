f9e294cd04261e8469b03c0ce496bbd29dcaedf8
Wu Jing <wujing@jike.com>
2012-05-09 14:14:50 +0800

    fetion login

--- src/com/race604/sms/SettingsActivity.java
+++ src/com/race604/sms/SettingsActivity.java
@@ -1,28 +1,41 @@
 package com.race604.sms;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TableRow;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.race604.fetion.client.FetionClient;
+import com.race604.fetion.client.ImageVerifyEvent;
+import com.race604.fetion.client.LoginStateEvent;
+import com.race604.fetion.client.NotifyEvent;
+import com.race604.fetion.client.NotifyEventListener;
+import com.race604.fetion.data.LoginState;
 import com.race604.fetion.data.Presence;
+import com.race604.fetion.data.VerifyImage;
 
 /**
  * @author Wu Jing wujing@jike.com
  * @version Create at：2012-5-8 下午3:15:54
  * 
  **/
-public class SettingsActivity extends SherlockActivity implements OnClickListener {
+public class SettingsActivity extends SherlockActivity implements OnClickListener, NotifyEventListener {
 
 	private EditText mAccountEt;
 	private EditText mPasswordEt;
 	private CheckBox mHiddenCb;
+	private TableRow mImgVerifyTr;
+	private ImageView mVerifyImgIv;
+	private EditText mVerifyCodeEt;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +54,10 @@ public class SettingsActivity extends SherlockActivity implements OnClickListene
 		mAccountEt = (EditText) findViewById(R.id.et_fetion_account);
 		mPasswordEt = (EditText) findViewById(R.id.et_fetion_pass);
 		mHiddenCb = (CheckBox) findViewById(R.id.cb_fetion_presence);
+		
+		mImgVerifyTr = (TableRow) findViewById(R.id.tr_verify);
+		mVerifyImgIv = (ImageView) findViewById(R.id.iv_verify_img);
+		mVerifyCodeEt = (EditText) findViewById(R.id.et_verify_code);
 	}
 
 	@Override
@@ -67,6 +84,7 @@ public class SettingsActivity extends SherlockActivity implements OnClickListene
 		
 		FetionClient fetionClient;
 		fetionClient = new FetionClient(account, pass);
+		fetionClient.setNotifyEventListener(this);
 		SmsApplication.get().setFetionClient(fetionClient);
 		fetionClient.login(mHiddenCb.isChecked() ? Presence.HIDEN : Presence.ONLINE);
 
@@ -80,5 +98,45 @@ public class SettingsActivity extends SherlockActivity implements OnClickListene
 			break;
 		}
 		
+	}
+
+	@Override
+	public void fireEvent(NotifyEvent event) {
+		switch(event.getEventType()){
+		case IMAGE_VERIFY: {
+			ImageVerifyEvent e10 = (ImageVerifyEvent) event;
+			imageVerify(e10.getVerifyImage(), e10.getVerifyReason(), e10.getVerifyTips(), e10);
+			break;
+		}
+		case LOGIN_STATE:
+			LoginStateEvent e7 = (LoginStateEvent) event;
+			loginStateChanged(e7.getLoginState());
+			break;
+			
+		}
+		
+	}
+	
+	/**
+	 * 处理验证码
+     * @param verifyImage		验证图片
+     * @param verifyReason		验证原因
+     * @param verifyTips		验证提示
+     */
+	private void imageVerify(VerifyImage verifyImage, String verifyReason,
+            String verifyTips, ImageVerifyEvent event)
+    {
+		mImgVerifyTr.setVisibility(View.VISIBLE);
+		byte[] imgData = verifyImage.getImageData();
+		Bitmap bitmap = BitmapFactory.decodeByteArray(imgData, 0, imgData.length);
+		mVerifyImgIv.setImageBitmap(bitmap);
+    }
+	
+	/**
+	 * 登陆状态发生了改变
+	 * @param state
+	 */
+	private void loginStateChanged(LoginState state)
+	{
 	}
 }
